8a5c0d0e8324d9defd670a0c440675c0baefe9a5
Mark <mortega@utexas.edu>
2014-08-14 19:25:00 -0500

    Mailer Generator

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Provide better details into the environment
+ActiveRecord::Base.logger.level = Logger::DEBUG
