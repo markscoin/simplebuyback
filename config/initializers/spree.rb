5f42022589658768c30f6e392be7077f220a7c63
Mark Ortega <mortega@utexas.edu>
2014-11-12 23:10:36 -0600

    Fixed product page issue and changed layout

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -8,7 +8,7 @@
 Spree.config do |config|
 
   config.show_variant_full_price = true
-  config.allow_guest_checkout = false
+  config.allow_guest_checkout = true
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
